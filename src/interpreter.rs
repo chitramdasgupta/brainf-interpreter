0d3c537d65a8301db5fdb5f08ccda01a3a06a390
Princic-1837592 <andreapri.99@gmail.com>
2022-07-05 20:14:25 +0200

    Added brackets check, `from` constructor, and simplified execution
    Added the `from` function for when you want to initialize the `Machine` with a specified length for the `data_tape.`
    Implemented a brackets check while parsing the file: now brackets are matched in pairs and an error is returned if some are mismatched.
    Simplified adds and subs. Simplified jumping between brackets.
    Now `parse_instructions` resets values to support multiple executions.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,106 +1,100 @@
+use std::collections::HashMap;
 use std::io::Read;
+
 
 const TAPE_LENGTH: usize = 30_000;
 
-pub struct Machine {
-    instruction_tape: Vec<char>,
-    data_tape: [u8; TAPE_LENGTH],
-    data_pointer: usize,
+
+#[derive(Debug)]
+pub enum Error {
+    UnmatchedClosedBracket(usize),
+    UnmatchedOpenBracket(usize),
 }
 
+
+pub struct Machine {
+    instruction_tape: Vec<char>,
+    data_tape: Vec<u8>,
+    data_pointer: usize,
+    brackets: HashMap<usize, usize>,
+}
+
+
 impl Machine {
-    pub fn new() -> Self {
+    pub fn from(tape_length: usize) -> Self {
         Self {
-            instruction_tape: Vec::new(),
-            data_tape: [0; TAPE_LENGTH],
+            instruction_tape: vec![],
+            data_tape: vec![0; tape_length],
             data_pointer: 0,
+            brackets: HashMap::new(),
         }
     }
 
-    pub fn parse_instructions(&mut self, contents: String) {
+
+    pub fn new() -> Self {
+        Self::from(TAPE_LENGTH)
+    }
+
+    pub fn parse_instructions(&mut self, contents: String) -> Result<(), Error> {
         self.instruction_tape = contents
             .chars()
             .filter(|c| matches!(*c, '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']'))
             .collect();
+        self.brackets.clear();
+        self.check_brackets()?;
+        self.data_tape = vec![0; self.data_tape.len()];
+        self.data_pointer = 0;
+        Ok(())
+    }
+
+    fn check_brackets(&mut self) -> Result<(), Error> {
+        let mut stack = vec![];
+        for (i, c) in self.instruction_tape.iter().enumerate() {
+            match c {
+                '[' => {
+                    stack.push(i);
+                }
+                ']' => {
+                    if stack.is_empty() {
+                        return Err(Error::UnmatchedClosedBracket(i));
+                    }
+                    let start = stack.pop().unwrap();
+                    self.brackets.insert(start, i);
+                    self.brackets.insert(i, start);
+                }
+                _ => {}
+            }
+        }
+        if stack.is_empty() {
+            Ok(())
+        } else {
+            Err(Error::UnmatchedOpenBracket(stack.pop().unwrap()))
+        }
     }
 
     pub fn execute_instructions(&mut self) {
         let mut i = 0;
         while i < self.instruction_tape.len() {
             match self.instruction_tape[i] {
-                '+' => {
-                    self.data_tape[self.data_pointer] = if self.data_tape[self.data_pointer] == 255
-                    {
-                        0
-                    } else {
-                        self.data_tape[self.data_pointer] + 1
-                    }
-                }
+                '+' => self.data_tape[self.data_pointer] = self.data_tape[self.data_pointer].wrapping_add(1),
+                '-' => self.data_tape[self.data_pointer] = self.data_tape[self.data_pointer].wrapping_sub(1),
 
-                '-' => {
-                    self.data_tape[self.data_pointer] = if self.data_tape[self.data_pointer] == 0 {
-                        255
-                    } else {
-                        self.data_tape[self.data_pointer] - 1
-                    }
-                }
-
-                '>' => {
-                    self.data_pointer += 1;
-                    self.data_pointer = if self.data_pointer == TAPE_LENGTH {
-                        0
-                    } else {
-                        self.data_pointer
-                    }
-                }
-
-                '<' => {
-                    self.data_pointer = if self.data_pointer == 0 {
-                        TAPE_LENGTH - 1
-                    } else {
-                        self.data_pointer - 1
-                    }
-                }
+                '>' => self.data_pointer = self.data_pointer.wrapping_add(1),
+                '<' => self.data_pointer = self.data_pointer.wrapping_sub(1),
 
                 '.' => print!("{}", self.data_tape[self.data_pointer] as char),
+                ',' => self.data_tape[self.data_pointer] = std::io::stdin()
+                    .bytes()
+                    .next()
+                    .and_then(|result| result.ok())
+                    .unwrap(),
 
-                ',' => {
-                    self.data_tape[self.data_pointer] = std::io::stdin()
-                        .bytes()
-                        .next()
-                        .and_then(|result| result.ok())
-                        .unwrap();
-                }
-
-                '[' => {
-                    if self.data_tape[self.data_pointer] == 0 {
-                        let mut nested_bracket = 0;
-                        i += 1;
-                        while nested_bracket > 0 || self.instruction_tape[i] != ']' {
-                            if self.instruction_tape[i] == '[' {
-                                nested_bracket += 1;
-                            } else if self.instruction_tape[i] == ']' {
-                                nested_bracket -= 1;
-                            }
-                            i += 1;
-                        }
-                    }
-                }
-
-                ']' => {
-                    if self.data_tape[self.data_pointer] != 0 {
-                        let mut nested_bracket = 0;
-                        i -= 1;
-                        while nested_bracket > 0 || self.instruction_tape[i] != '[' {
-                            if self.instruction_tape[i] == ']' {
-                                nested_bracket += 1;
-                            } else if self.instruction_tape[i] == '[' {
-                                nested_bracket -= 1;
-                            }
-                            i -= 1;
-                        }
-                    }
-                }
+                '[' => if self.data_tape[self.data_pointer] == 0 {
+                    i = self.brackets[&i];
+                },
+                ']' => if self.data_tape[self.data_pointer] != 0 {
+                    i = self.brackets[&i];
+                },
 
                 _ => {}
             }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::{env, fs};
+
+
 mod interpreter;
 
-fn main() {
+
+fn main() -> Result<(), interpreter::Error> {
     let args: Vec<String> = env::args().collect();
     let file_name = &args[1];
     let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file");
 
     let mut machine = interpreter::Machine::new();
-    machine.parse_instructions(contents);
+    machine.parse_instructions(contents)?;
     machine.execute_instructions();
+    Ok(())
 }
