b30476da503e1a8b21d4fc0218ece0bf60d587a9
chitramdasgupta <chitramdasgupta@gmail.com>
2022-07-05 21:06:16 +0530

    Strong typed the instructions

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,22 +1,34 @@
-use std::{io::Read, fmt};
+use std::{fmt, io::Read};
 
 const TAPE_LENGTH: usize = 30_000;
 
+#[derive(Clone, Hash, Debug, PartialEq)]
+enum Instr {
+    IncrDataByte,
+    DecrDataByte,
+    IncrDataPointer,
+    DecrDataPointer,
+    Print,
+    Input,
+    JumpForward,
+    JumpBackward,
+}
+
 #[derive(Clone, Hash)]
 pub struct Machine {
-    instruction_tape: Vec<char>,
+    instruction_tape: Vec<Instr>,
     data_tape: [u8; TAPE_LENGTH],
     data_pointer: usize,
 }
 
 impl Default for Machine {
-    fn default() -> Self { 
+    fn default() -> Self {
         Self {
             instruction_tape: Vec::new(),
             data_tape: [0; TAPE_LENGTH],
             data_pointer: 0,
         }
-     }
+    }
 }
 
 impl fmt::Debug for Machine {
@@ -31,75 +43,72 @@ impl fmt::Debug for Machine {
 
 impl fmt::Display for Machine {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Machine (instruction_tape: {:?}, data_tape: {:?}, data_ponter: {})", 
-            self.instruction_tape, self.data_tape, self.data_pointer)
+        write!(
+            f,
+            "Machine (instruction_tape: {:?}, data_tape: {:?}, data_ponter: {})",
+            self.instruction_tape, self.data_tape, self.data_pointer
+        )
     }
 }
 
 impl Machine {
     pub fn parse_instructions(&mut self, contents: String) {
-        self.instruction_tape = contents
-            .chars()
-            .filter(|c| matches!(*c, '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']'))
-            .collect();
+        contents.chars().for_each(|c| {
+            match c {
+                '+' => self.instruction_tape.push(Instr::IncrDataByte),
+                '-' => self.instruction_tape.push(Instr::DecrDataByte),
+                '>' => self.instruction_tape.push(Instr::IncrDataPointer),
+                '<' => self.instruction_tape.push(Instr::DecrDataPointer),
+                '.' => self.instruction_tape.push(Instr::Print),
+                ',' => self.instruction_tape.push(Instr::Input),
+                '[' => self.instruction_tape.push(Instr::JumpForward),
+                ']' => self.instruction_tape.push(Instr::JumpBackward),
+                _ => {}
+            };
+        });
     }
 
     pub fn execute_instructions(&mut self) {
         let mut i = 0;
         while i < self.instruction_tape.len() {
             match self.instruction_tape[i] {
-                '+' => {
-                    self.data_tape[self.data_pointer] = if self.data_tape[self.data_pointer] == 255
-                    {
-                        0
-                    } else {
-                        self.data_tape[self.data_pointer] + 1
-                    }
+                Instr::IncrDataByte => {
+                    self.data_tape[self.data_pointer] =
+                        self.data_tape[self.data_pointer].wrapping_add(1)
                 }
 
-                '-' => {
-                    self.data_tape[self.data_pointer] = if self.data_tape[self.data_pointer] == 0 {
-                        255
-                    } else {
-                        self.data_tape[self.data_pointer] - 1
-                    }
+                Instr::DecrDataByte => {
+                    self.data_tape[self.data_pointer] =
+                        self.data_tape[self.data_pointer].wrapping_sub(1)
                 }
 
-                '>' => {
-                    self.data_pointer += 1;
-                    self.data_pointer = if self.data_pointer == TAPE_LENGTH {
-                        0
-                    } else {
-                        self.data_pointer
-                    }
+                Instr::IncrDataPointer => {
+                    self.data_pointer = (self.data_pointer + 1).rem_euclid(TAPE_LENGTH)
                 }
 
-                '<' => {
-                    self.data_pointer = if self.data_pointer == 0 {
-                        TAPE_LENGTH - 1
-                    } else {
-                        self.data_pointer - 1
-                    }
+                Instr::DecrDataPointer => {
+                    self.data_pointer = (self.data_pointer - 1).rem_euclid(TAPE_LENGTH)
                 }
 
-                '.' => print!("{}", self.data_tape[self.data_pointer] as char),
+                Instr::Print => print!("{}", self.data_tape[self.data_pointer] as char),
 
-                ',' => {
+                Instr::Input => {
                     self.data_tape[self.data_pointer] = std::io::stdin()
                         .bytes()
                         .next()
                         .and_then(|result| result.ok())
-                        .unwrap();
+                        .expect("Could not read input");
                 }
 
-                '[' => {
+                Instr::JumpForward => {
                     if self.data_tape[self.data_pointer] == 0 {
                         let mut nested_bracket = 0;
                         i += 1;
-                        while nested_bracket > 0 || self.instruction_tape[i] != ']' {
-                            if self.instruction_tape[i] == '[' {
+                        while nested_bracket > 0 || self.instruction_tape[i] != Instr::JumpBackward
+                        {
+                            if self.instruction_tape[i] == Instr::JumpForward {
                                 nested_bracket += 1;
-                            } else if self.instruction_tape[i] == ']' {
+                            } else if self.instruction_tape[i] == Instr::JumpBackward {
                                 nested_bracket -= 1;
                             }
                             i += 1;
@@ -107,22 +116,20 @@ impl Machine {
                     }
                 }
 
-                ']' => {
+                Instr::JumpBackward => {
                     if self.data_tape[self.data_pointer] != 0 {
                         let mut nested_bracket = 0;
                         i -= 1;
-                        while nested_bracket > 0 || self.instruction_tape[i] != '[' {
-                            if self.instruction_tape[i] == ']' {
+                        while nested_bracket > 0 || self.instruction_tape[i] != Instr::JumpForward {
+                            if self.instruction_tape[i] == Instr::JumpBackward {
                                 nested_bracket += 1;
-                            } else if self.instruction_tape[i] == '[' {
+                            } else if self.instruction_tape[i] == Instr::JumpForward {
                                 nested_bracket -= 1;
                             }
                             i -= 1;
                         }
                     }
                 }
-
-                _ => {}
             }
             i += 1;
         }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::{env, fs};
 mod interpreter;
 
 fn main() {
-    let file_name = env::args().nth(1).expect("Please enter the path to the source");
+    let file_name = env::args()
+        .nth(1)
+        .expect("Please enter the path to the source");
     let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file");
 
     let mut machine = interpreter::Machine::default();
