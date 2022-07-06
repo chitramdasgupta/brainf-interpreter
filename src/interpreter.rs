7d2d583a9595d2d699775742754ba9d0fccd12a8
Princic-1837592 <andreapri.99@gmail.com>
2022-07-06 08:26:21 +0200

    Updated to new `Instr` enum
    Updated to support the new enum to represent instructions.
    Removed (now useless) hashmap to keep track of brackets: now they are managed by `Instr::JumpForward(usize)` and `Instr::JumpBackward(usize)`

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,8 +1,20 @@
-use std::collections::HashMap;
-use std::io::Read;
+use std::{fmt, io::Read};
 
 
-const TAPE_LENGTH: usize = 30_000;
+const TAPE_LEN: usize = 30_000;
+
+
+#[derive(Clone, Hash, Debug, PartialEq)]
+enum Instr {
+    IncrDataByte,
+    DecrDataByte,
+    IncrDataPointer,
+    DecrDataPointer,
+    Print,
+    Input,
+    JumpForward(usize),
+    JumpBackward(usize),
+}
 
 
 #[derive(Debug)]
@@ -12,11 +24,44 @@ pub enum Error {
 }
 
 
+#[derive(Clone)]
 pub struct Machine {
-    instruction_tape: Vec<char>,
+    instruction_tape: Vec<Instr>,
     data_tape: Vec<u8>,
     data_pointer: usize,
-    brackets: HashMap<usize, usize>,
+}
+
+
+impl Default for Machine {
+    fn default() -> Self {
+        Self {
+            instruction_tape: Default::default(),
+            data_tape: vec![0; TAPE_LEN],
+            data_pointer: Default::default(),
+        }
+    }
+}
+
+
+impl fmt::Debug for Machine {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Machine")
+         .field("instr_tape", &self.instruction_tape)
+         .field("data", &self.data_tape)
+         .field("pointer", &self.data_pointer)
+         .finish()
+    }
+}
+
+
+impl fmt::Display for Machine {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Machine (instr_tape: {:?}, data: {:?}, pointer: {})",
+            self.instruction_tape, self.data_tape, self.data_pointer
+        )
+    }
 }
 
 
@@ -26,21 +71,29 @@ impl Machine {
             instruction_tape: vec![],
             data_tape: vec![0; tape_length],
             data_pointer: 0,
-            brackets: HashMap::new(),
         }
     }
 
 
     pub fn new() -> Self {
-        Self::from(TAPE_LENGTH)
+        Self::from(TAPE_LEN)
     }
 
     pub fn parse_instructions(&mut self, contents: String) -> Result<(), Error> {
         self.instruction_tape = contents
             .chars()
-            .filter(|c| matches!(*c, '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']'))
+            .filter_map(|c| match c {
+                '+' => Some(Instr::IncrDataByte),
+                '-' => Some(Instr::DecrDataByte),
+                '>' => Some(Instr::IncrDataPointer),
+                '<' => Some(Instr::DecrDataPointer),
+                '.' => Some(Instr::Print),
+                ',' => Some(Instr::Input),
+                '[' => Some(Instr::JumpForward(0)),
+                ']' => Some(Instr::JumpBackward(0)),
+                _ => None
+            })
             .collect();
-        self.brackets.clear();
         self.check_brackets()?;
         self.data_tape = vec![0; self.data_tape.len()];
         self.data_pointer = 0;
@@ -49,18 +102,18 @@ impl Machine {
 
     fn check_brackets(&mut self) -> Result<(), Error> {
         let mut stack = vec![];
-        for (i, c) in self.instruction_tape.iter().enumerate() {
+        for (i, c) in self.instruction_tape.clone().iter().enumerate() {
             match c {
-                '[' => {
+                Instr::JumpForward(0) => {
                     stack.push(i);
                 }
-                ']' => {
+                Instr::JumpBackward(0) => {
                     if stack.is_empty() {
                         return Err(Error::UnmatchedClosedBracket(i));
                     }
                     let start = stack.pop().unwrap();
-                    self.brackets.insert(start, i);
-                    self.brackets.insert(i, start);
+                    self.instruction_tape[i] = Instr::JumpBackward(start);
+                    self.instruction_tape[start] = Instr::JumpForward(i);
                 }
                 _ => {}
             }
@@ -76,27 +129,25 @@ impl Machine {
         let mut i = 0;
         while i < self.instruction_tape.len() {
             match self.instruction_tape[i] {
-                '+' => self.data_tape[self.data_pointer] = self.data_tape[self.data_pointer].wrapping_add(1),
-                '-' => self.data_tape[self.data_pointer] = self.data_tape[self.data_pointer].wrapping_sub(1),
+                Instr::IncrDataByte => self.data_tape[self.data_pointer] = self.data_tape[self.data_pointer].wrapping_add(1),
+                Instr::DecrDataByte => self.data_tape[self.data_pointer] = self.data_tape[self.data_pointer].wrapping_sub(1),
 
-                '>' => self.data_pointer = self.data_pointer.wrapping_add(1),
-                '<' => self.data_pointer = self.data_pointer.wrapping_sub(1),
+                Instr::IncrDataPointer => self.data_pointer = self.data_pointer.wrapping_add(1),
+                Instr::DecrDataPointer => self.data_pointer = self.data_pointer.wrapping_sub(1),
 
-                '.' => print!("{}", self.data_tape[self.data_pointer] as char),
-                ',' => self.data_tape[self.data_pointer] = std::io::stdin()
+                Instr::Print => print!("{}", self.data_tape[self.data_pointer] as char),
+                Instr::Input => self.data_tape[self.data_pointer] = std::io::stdin()
                     .bytes()
                     .next()
                     .and_then(|result| result.ok())
                     .unwrap(),
 
-                '[' => if self.data_tape[self.data_pointer] == 0 {
-                    i = self.brackets[&i];
+                Instr::JumpForward(jump) => if self.data_tape[self.data_pointer] == 0 {
+                    i = jump;
                 },
-                ']' => if self.data_tape[self.data_pointer] != 0 {
-                    i = self.brackets[&i];
+                Instr::JumpBackward(jump) => if self.data_tape[self.data_pointer] != 0 {
+                    i = jump;
                 },
-
-                _ => {}
             }
             i += 1;
         }
