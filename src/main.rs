6cd2464dbcae08fcaa14704043e8348b3a726768
chitramdasgupta <chitramdasgupta@gmail.com>
2022-07-06 17:55:00 +0530

    Cleaned up taking input for the number of cells

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ mod interpreter;
 fn main() -> Result<(), interpreter::Error> {
     let args: Vec<String> = env::args().collect();
 
-    let file_name = &args[1];
+    let file_name = &args
+        .get(1)
+        .unwrap();
 
-    let mut cell_count: usize = 30_000;
-    if args.len() == 3 {
-        cell_count = (&args[2]).parse::<usize>().unwrap_or(30_000);
-    }
+    let cell_count = match &args.get(2) {
+        Some(count) => count.parse::<usize>().unwrap(),
+        None => 30_000,
+    };
 
     let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file");
 
